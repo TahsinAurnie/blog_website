73abea7dae907d196b86a96cd4c11879ef7e3ce8
Tahsin Fateha Aurnie <tfaurnie99@gmail.com>
2023-10-30 16:33:04 +0600

    admin view change

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,9 @@ from .models import *
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Blog)
+# admin.site.register(Blog)
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title')
 admin.site.register(Comment)
 admin.site.register(Reply)
--- user_profile/admin.py
+++ user_profile/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(User)
+# admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','username', 'email')
 admin.site.register(Follow)
